63929a4117f4f65f64d9861026bcebf2597838af
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-10-20 18:13:38 +0000

    added form with input feilds

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,27 @@ import './App.css'
 
 export default function App() {
   return (
-    <div>
-      Hello World
+    <div className='App'>
+      <div className='container'>
+      <form>
+        <input
+          type="text"
+          placeholder="First Name"
+          />
+
+        <input
+          type="text"
+          placeholder="Last Name"
+          />
+
+        <input
+          type="text"
+          placeholder="Email"
+          />
+
+        <button type="submit">Register</button>
+      </form>
+      </div>
     </div>
   )
 }
