6fbe2005727076f16b54cb1c49eef12df977bf7b
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2023-10-20 18:44:48 +0000

    Added css styling to the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ export default function App() {
     <div className='App'>
       <div className='container'>
       <form>
-        <input
+        <div className="ip1">
+          <input
           type="text"
           placeholder="First Name"
           />
-
         <input
           type="text"
           placeholder="Last Name"
           />
-
         <input
           type="text"
           placeholder="Email"
           />
+          </div>
 
+        <div className="ip2">
         <button type="submit">Register</button>
+          </div>
       </form>
       </div>
     </div>
